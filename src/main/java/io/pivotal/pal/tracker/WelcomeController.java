e4e6a5dcfb4f0dce6ae7e1a6caefa89d4d5d2a4d
e050246 <bret.deasy@mastercard.com>
2018-01-30 13:05:35 -0500

    Fixed message body to 'hello'

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -6,6 +6,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController public class WelcomeController {
 
     @GetMapping("/") public String sayHello() {
-        return "Hello";
+        return "hello";
     }
 }
